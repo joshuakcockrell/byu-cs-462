15757428bbca9aa1b9453e9c7c3420bb62f1b409
joshuakcockrell <joshuakcockrell@gmail.com>
2017-12-05 12:52:14 -0700

    Change logging

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+const fs = require('fs');
+const util = require('util');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
+
+// Override console.log to write to output.log file
+const log_file = fs.createWriteStream(__dirname + '/output.log', {flags : 'w'});
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+};
 
 app.get('/', function (req, res) {
   console.log('Index');
