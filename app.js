fa5fe29f9cc034bf38a35a1893205ace810286b1
joshuakcockrell <joshuakcockrell@gmail.com>
2017-12-07 16:38:37 -0700

    Public files

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const path = require('path');
 
 console.log('starting at:');
 console.log(__dirname);
+
 // Override console.log to write to output.log file
 const log_file = fs.createWriteStream(__dirname + '/output.log', {flags : 'a'});
 console.log = function(d) { //
   log_file.write(util.format(d) + '\n');
 };
+
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   console.log('Index');
