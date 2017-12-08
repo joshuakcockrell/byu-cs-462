9a9d9313c1aea3b4fcfca3d0236912ec92fdc8aa
joshuakcockrell <joshuakcockrell@gmail.com>
2017-12-07 17:06:01 -0700

    Work

--- app.js
+++ app.js
@@ -8,18 +8,29 @@ const path = require('path');
 console.log('starting at:');
 console.log(__dirname);
 
-// Override console.log to write to output.log file
-const log_file = fs.createWriteStream(__dirname + '/output.log', {flags : 'a'});
-console.log = function(d) { //
-  log_file.write(util.format(d) + '\n');
-};
+// // Override console.log to write to output.log file
+// const log_file = fs.createWriteStream(__dirname + '/output.log', {flags : 'a'});
+// console.log = function(d) { //
+//   log_file.write(util.format(d) + '\n');
+// };
 
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   console.log('Index');
   res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/test', function (req, res) {
+  console.log('GET');
+  res.json({query: req.query, body: req.body});
+});
+
+app.post('/test', function (req, res) {
+  console.log('POST');
+  res.json({query: req.query, body: req.body, headers: req.headers});
 });
 
 var server = app.listen(8000, function() {
