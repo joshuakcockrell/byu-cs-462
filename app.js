423a34414e11d8bfd25bdd1335bb444d32d00dc5
joshuakcockrell <joshuakcockrell@gmail.com>
2017-12-12 16:29:41 -0700

    a

--- app.js
+++ app.js
@@ -85,10 +85,7 @@ app.get('/view/:id', (req, res) => {
   }
 
   // If logged in as this user
-  console.log('--------');
-  console.log(''+user.id);
-  console.dir(''+req.cookies.user.id);
-  if (''+user.id === ''+req.cookies.user.id) {
+  if (req.cookies.user !== undefined && (''+user.id === ''+req.cookies.user.id)) {
     if (user.checkins !== undefined && user.checkins.count > 0) {
       let checkins = user.checkins.items;
       text += '<h2>Checkins</h2>';
