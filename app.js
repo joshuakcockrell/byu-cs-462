e79ffe8ed865902444b64d0a139ea6d33bfa8c63
joshuakcockrell <joshuakcockrell@gmail.com>
2017-12-13 12:49:27 -0700

    a

--- app.js
+++ app.js
@@ -348,58 +348,76 @@
 
 
 
-function approveDomains(opts, certs, cb) {
-  // This is where you check your database and associated
-  // email addresses with domains and agreements and such
- 
-  // The domains being approved for the first time are listed in opts.domains
-  // Certs being renewed are listed in certs.altnames
-  if (certs) {
-    opts.domains = certs.altnames;
-  }
-  else {
-    opts.email = 'joshuakcockrell@gmail.com';
-    opts.agreeTos = true;
-  }
- 
-  // NOTE: you can also change other options such as `challengeType` and `challenge`
-  // opts.challengeType = 'http-01';
-  // opts.challenge = require('le-challenge-fs').create({});
- 
-  cb(null, { options: opts, certs: certs });
-}
 
-var lex = require('greenlock-express').create({
-  // set to https://acme-v01.api.letsencrypt.org/directory in production
-  server: 'staging'
- 
-// If you wish to replace the default plugins, you may do so here
-//
-, challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
-, store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
- 
-// You probably wouldn't need to replace the default sni handler
-// See https://git.coolaj86.com/coolaj86/le-sni-auto if you think you do
-//, sni: require('le-sni-auto').create({})
- 
-, approveDomains: approveDomains
-});
 
-// handles acme-challenge and redirects to https
-require('http').createServer(lex.middleware(require('redirect-https')())).listen(8888, function () {
-  console.log("Listening for ACME http-01 challenges on", this.address());
-});
- 
-var app = require('express')();
-app.use('/', function (req, res) {
-  res.end('Hello, World!');
-});
- 
-// handles your app
-require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(8000, function () {
-  console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
-});
 
+
+
+
+
+
+
+
+// function approveDomains(opts, certs, cb) {
+//   // This is where you check your database and associated
+//   // email addresses with domains and agreements and such
+ 
+//   // The domains being approved for the first time are listed in opts.domains
+//   // Certs being renewed are listed in certs.altnames
+//   if (certs) {
+//     opts.domains = certs.altnames;
+//   }
+//   else {
+//     opts.email = 'joshuakcockrell@gmail.com';
+//     opts.agreeTos = true;
+//   }
+ 
+//   // NOTE: you can also change other options such as `challengeType` and `challenge`
+//   // opts.challengeType = 'http-01';
+//   // opts.challenge = require('le-challenge-fs').create({});
+ 
+//   cb(null, { options: opts, certs: certs });
+// }
+
+// var lex = require('greenlock-express').create({
+//   // set to https://acme-v01.api.letsencrypt.org/directory in production
+//   server: 'staging'
+ 
+// // If you wish to replace the default plugins, you may do so here
+// //
+// , challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
+// , store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
+ 
+// // You probably wouldn't need to replace the default sni handler
+// // See https://git.coolaj86.com/coolaj86/le-sni-auto if you think you do
+// //, sni: require('le-sni-auto').create({})
+ 
+// , approveDomains: approveDomains
+// });
+
+// // handles acme-challenge and redirects to https
+// require('http').createServer(lex.middleware(require('redirect-https')())).listen(8888, function () {
+//   console.log("Listening for ACME http-01 challenges on", this.address());
+// });
+ 
+// var app = require('express')();
+// app.use('/', function (req, res) {
+//   res.end('Hello, World!');
+// });
+ 
+// // handles your app
+// require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(8000, function () {
+//   console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
+// });
+
+
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(8000, () => console.log('Example app listening on port 8000!'))
 
 
 
